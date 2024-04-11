ef305f498491dd7b1835f9cbcf12d64818b0bd78
Philippe Fanaro <philippefanaro@gmail.com>
2024-04-11 17:01:05 -0300

    configuring the new Logitech MX Vertical mouse

--- scripts/kbd_base.sh
+++ scripts/kbd_base.sh
@@ -2,6 +2,10 @@ xset r rate 300 40
 xset s off
 xset -dpms
 
+# Changing the Mouse Speed
+# For more info, check [this answer](https://unix.stackexchange.com/a/177640/364698).
+xinput set-prop 13 "Coordinate Transformation Matrix" 0.70 0 0 0 0.7 0 0 0 1
+
 imwheel
 
 setxkbmap us
