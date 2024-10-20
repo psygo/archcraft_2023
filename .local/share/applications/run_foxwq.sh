f91c23409ce59a005eadc09772222d53857bdf1b
Philippe Fanaro <philippefanaro@gmail.com>
2024-10-20 16:45:37 -0300

    adding some documentation on how I did the last thing

--- .local/share/applications/run_foxwq.sh
+++ .local/share/applications/run_foxwq.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# See these questions for more info:
+#
+# - [Wine Converted App (Fox Weiqi) Not Showing Chinese (or CJK) Characters](https://unix.stackexchange.com/a/785322/364698)
+# - [Adding a Wine `.lnk` Program to Rofi](https://unix.stackexchange.com/a/784808/364698)
+
 cd "${HOME}/foxweiqi/drive_c/Program Files (x86)/foxwq/foxwq"
 
 WINEPREFIX=~/foxweiqi wine foxwq.exe
